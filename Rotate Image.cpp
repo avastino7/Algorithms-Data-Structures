ee299c65677669cf6a6e9d575251227d0ef4232c
shrut_512 <84448891+Shrut012@users.noreply.github.com>
2022-10-31 21:56:07 +0530

    Create Rotate Image.cpp
    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
    
    You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.

---
+++ Rotate Image.cpp
@@ -0,0 +1,21 @@
+/*
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+*/
+
+
+void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int a = 0;
+        int b = n-1;
+        while(a<b){
+            for(int i=0;i<(b-a);++i){
+                swap(matrix[a][a+i], matrix[a+i][b]);
+                swap(matrix[a][a+i], matrix[b][b-i]);
+                swap(matrix[a][a+i], matrix[b-i][a]);
+            }
+            ++a;
+            --b;
+        }
+    }
