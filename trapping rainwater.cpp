b4528631cc45564b71a6022c09006ae437009750
abhishekyadav13 <92470955+abhishekyadav13@users.noreply.github.com>
2022-10-31 19:42:06 +0530

    Complexity  Time: O(N), where N <= 3*10^4 is number of bars. Space: O(N)

---
+++ trapping rainwater.cpp
@@ -0,0 +1,27 @@
+/*
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+*/
+
+class Solution { // 0 ms, faster than 100.00%
+    public int trap(int[] height) {
+        if (height.length <= 2) return 0;
+        int n = height.length, maxLeft = height[0], maxRight = height[n-1];
+        int left = 1, right = n - 2, ans = 0;
+        while (left <= right) {
+            if (maxLeft < maxRight) {
+                if (height[left] > maxLeft)
+                    maxLeft = height[left];
+                else
+                    ans += maxLeft - height[left];
+                left += 1;
+            } else {
+                if (height[right] > maxRight)
+                    maxRight = height[right];
+                else
+                    ans += maxRight - height[right];
+                right -= 1;
+            }
+        }
+        return ans;
+    }
+}
